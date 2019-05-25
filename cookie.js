66f0d5e5d98ba80ede297b7ef22656b4a4962848
Frankie-fu <49645448+Frankie-fu@users.noreply.github.com>
2019-05-25 21:46:00 +0800

    Add files via upload

--- cookie.js
+++ cookie.js
@@ -1,6 +1,7 @@
 var c = document.getElementById('mycanvas');
 var ctx = c.getContext('2d');
 
+var increment=1;
 var cookies=0;
 var s = document.getElementById('score');
 s.innerHTML= cookies;
@@ -8,16 +9,48 @@ s.innerHTML= cookies;
 c.addEventListener("click",draw,true);
 
 function draw(event) {
-    cookies=cookies+1;
+    cookies=cookies+increment;
     s.innerHTML=cookies;
     
+    var aud = document.getElementById("myAudio");
+    aud.play();
+    
+    if (cookies == 20) {
+        increment=2;
+        award(increment,"gray")
+    }
+    
+    if (cookies == 50) {
+        increment=3;
+        award(increment,"blue")
+    }
+    
+    if (cookies == 101) {
+        increment=4;
+        award(increment,"green")
+    }
+    
+     if (cookies == 201) {
+        increment=5;
+        award(increment,"gold")
+    }
+    function award(increment,colour) {
+    mytab = "<table><tr>";
+    mytab = mytab + "<td style='background-color:" + colour +
+";padding:25px; color:white'>";
+    mytab = mytab + "Congratulations!" + increment + "-pointers!";
+    mytab = mytab + "</td>";
+    mytab = mytab + "</tr></table>";
+    mydiv = document.getElementById("store");
+    mydiv.innerHTML = mytab;
+}
     var alpha=1.0;
     y = event.clientY
     interval = setInterval (function (){
     ctx.globalAlpha=alpha;
     ctx.font = "normal 20px Arial";
     ctx.fillStyle = "white";
-    ctx.fillText("+1",event.clientX,y);
+    ctx.fillText("+"+increment,event.clientX,y);
     alpha = alpha - 0.05;
     y=y-5;
     if (alpha <= 0) {
